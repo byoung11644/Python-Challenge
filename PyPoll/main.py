eaab28339515bb53fb9cebad0425071516d95b4c
Brian Young <Brian.Young@sacw.com>
2020-10-03 09:05:24 -0500

    figured out the candidate votes

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -8,15 +8,29 @@ with open(csvpath) as csvfile:
     csv_header = next(csvreader)
     
     totalVotes = 0
-    candidates = []
+    candidatesList = []
+    candidateVotes = {i : 0 for i in candidatesList}
     
     for row in csvreader:
         totalVotes = totalVotes + 1
         
-        if row[2] not in candidates:
-            candidates.append(row[2])
+        if row[2] not in candidatesList:
+            candidatesList.append(row[2])
+            candidateVotes[row[2]] = 1
+        else:
+            candidateVotes[row[2]] = int(candidateVotes[row[2]]) + 1
+    
+            
+            
+            
+    
+    
+            
+            
         
         
         
-print(totalVotes)
-print(candidates)
+        
+#print(totalVotes)
+#print(candidatesList)
+print(candidateVotes)
