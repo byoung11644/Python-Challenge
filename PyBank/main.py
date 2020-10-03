e9252ab592645607a7d431e5badcbd53e9d7ec65
Brian Young <Brian.Young@sacw.com>
2020-10-02 22:40:09 -0500

    still trying to figure out the average of the changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,12 +9,32 @@ with open(csvpath) as csvfile:
     
     months = 0
     netProfit = 0
+    monthChange = []
+    profitlist = []
+    highestGain = 0
+    highestLoss = 0
     
     for row in csvreader:
         months = months + 1
         netProfit = float(row[1]) + netProfit
+            
+        if float(row[1]) > float(highestGain):
+            highestGain = row[1]
+        else:
+            highestGain = highestGain
+            
+        if float(row[1]) < float(highestLoss):
+            highestLoss = row[1]
+        else: highestLoss = highestLoss
+            
+                
+      
+
         
         
-    print('Total Months: ' + str(months))
-    print('Total Profit/Loss: $' + str(netProfit))
+    
+print('Total Months: ' + str(months))
+print('Total Profit/Loss: $' + str(netProfit))
+print('Greatest increase in Profits: $' + str(highestGain))
+print('Greatest decrease in Profits: $' + str(highestLoss))
     
