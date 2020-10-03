fd66c99322d68eaeff1a294082f2e2fb8d5c136b
Brian Young <Brian.Young@sacw.com>
2020-10-03 08:03:57 -0500

    second pypoll commit

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,4 +7,16 @@ with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
     
+    totalVotes = 0
+    candidates = []
     
+    for row in csvreader:
+        totalVotes = totalVotes + 1
+        
+        if row[2] not in candidates:
+            candidates.append(row[2])
+        
+        
+        
+print(totalVotes)
+print(candidates)
